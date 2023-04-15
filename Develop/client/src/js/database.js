beebf711cefd4a7d6effcebc6de50a95fbab279c
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-04-14 22:47:28 -0400

    changed get and return

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -19,20 +19,22 @@ console.log('Post to the ase');
   const result = await jateDb
   .transaction('jate', 'readwrite')
   .objectStore('jate')
-  .add({ jate: content });
-  console.log('🚀 - data saved to the database', result);
+  .add({ id: 1, jate: content });
+  console.log('🚀 - data saved to the database', result.jate);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// TODO: Add logic for a method that tgets all the content from the database
 export const getDb = async () => {
   console.log('GET all from the database');
   const jateDb = await openDB('jate', 1);
   const result = await jateDb
   .transaction('jate', 'readonly')
-  .objectStore('jate')
-  .getAll();
+  .objectStore('jate').get(1);
   console.log('result.value', result)
-  return result;
+
+  result ? console.log('data retrieved from db'): console.log('data not found')
+
+  return result?.jate;
 } 
 
 initdb();
