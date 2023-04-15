c4a715708260884f13a9122a6d4848457eae4d94
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-04-15 00:10:39 -0400

    fixing get bug

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -24,12 +24,12 @@ console.log('Post to the ase');
 };
 
 // TODO: Add logic for a method that tgets all the content from the database
-export const getDb = async () => {
+export const getDb = async (id) => {
   console.log('GET all from the database');
   const jateDb = await openDB('jate', 1);
   const result = await jateDb
   .transaction('jate', 'readonly')
-  .objectStore('jate').get(1);
+  .objectStore('jate').get(id);
   console.log('result.value', result)
 
   result ? console.log('data retrieved from db'): console.log('data not found')
